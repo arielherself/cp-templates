cf752da6536cb2334c97dbd673b9ee7a37223b58
Ariel <ariel@arielherself.xyz>
2024-01-21 10:06:30 +0800

    Update sa.cc

--- string/sa.cc
+++ string/sa.cc
@@ -1,59 +1,43 @@
-#include <algorithm>
-#include <cstdio>
-#include <cstring>
-#include <iostream>
+#include "../include.hh"
 
-using namespace std;
-
-const int N = 1000010;
+constexpr int N = 1e6 + 10;
 
 char s[N];
-// key1[i] = rk[id[i]]（作为基数排序的第一关键字数组）
 int n, sa[N], rk[N], oldrk[N << 1], id[N], key1[N], cnt[N], height[N];
 
 bool cmp(int x, int y, int w) {
   return oldrk[x] == oldrk[y] && oldrk[x + w] == oldrk[y + w];
 }
 
+void calc_sa() {
+    n = strlen(s + 1);
+    int i, m = 127, p, w;
+    for (i = 1; i <= n; ++i) ++cnt[rk[i] = s[i]];
+      for (i = 1; i <= m; ++i) cnt[i] += cnt[i - 1];
+      for (i = n; i >= 1; --i) sa[cnt[rk[i]]--] = i;
+
+      for (w = 1;; w <<= 1, m = p) {
+        for (p = 0, i = n; i > n - w; --i) id[++p] = i;
+        for (i = 1; i <= n; ++i)
+          if (sa[i] > w) id[++p] = sa[i] - w;
+
+        memset(cnt, 0, sizeof(cnt));
+        for (i = 1; i <= n; ++i) ++cnt[key1[i] = rk[id[i]]];
+
+        for (i = 1; i <= m; ++i) cnt[i] += cnt[i - 1];
+        for (i = n; i >= 1; --i) sa[cnt[key1[i]]--] = id[i];
+        memcpy(oldrk + 1, rk + 1, n * sizeof(int));
+        for (p = 0, i = 1; i <= n; ++i)
+          rk[sa[i]] = cmp(sa[i], sa[i - 1], w) ? p : ++p;
+        if (p == n) {
+          break;
+        }
+      }
+}
+
 int main() {
-  int i, m = 127, p, w;
-
-  scanf("%s", s + 1);
-  n = strlen(s + 1);
-
-  // calc sa[]
-  for (i = 1; i <= n; ++i) ++cnt[rk[i] = s[i]];
-  for (i = 1; i <= m; ++i) cnt[i] += cnt[i - 1];
-  for (i = n; i >= 1; --i) sa[cnt[rk[i]]--] = i;
-
-  for (w = 1;; w <<= 1, m = p) {  // m=p 就是优化计数排序值域
-    for (p = 0, i = n; i > n - w; --i) id[++p] = i;
-    for (i = 1; i <= n; ++i)
-      if (sa[i] > w) id[++p] = sa[i] - w;
-
-    memset(cnt, 0, sizeof(cnt));
-    for (i = 1; i <= n; ++i) ++cnt[key1[i] = rk[id[i]]];
-    // 注意这里px[i] != i，因为rk没有更新，是上一轮的排名数组
-
-    for (i = 1; i <= m; ++i) cnt[i] += cnt[i - 1];
-    for (i = n; i >= 1; --i) sa[cnt[key1[i]]--] = id[i];
-    memcpy(oldrk + 1, rk + 1, n * sizeof(int));
-    for (p = 0, i = 1; i <= n; ++i)
-      rk[sa[i]] = cmp(sa[i], sa[i - 1], w) ? p : ++p;
-    if (p == n) {
-      break;
-    }
-  }
-
-  // calc height[]
-  for (i = 1, k = 0; i <= n; ++i) {
-    if (rk[i] == 0) continue;
-    if (k) --k;
-    while (s[i + k] == s[sa[rk[i] - 1] + k]) ++k;
-    height[rk[i]] = k;
-  }
-
-  for (i = 1; i <= n; ++i) printf("%d ", sa[i]);
-
-  return 0;
+    untie;
+    cin >> (s + 1);  // array s starts from index 1
+    calc_sa();
+    for (int i = 1; i <= n; ++i) cout << sa[i] << " \n"[i == n];
 }
