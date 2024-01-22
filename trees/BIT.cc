9f5f1f60430d15d984e694d14c3b911933f6609a
Ariel <ariel@arielherself.xyz>
2024-01-22 17:09:44 +0800

    Create BIT.cc

---
+++ trees/BIT.cc
@@ -0,0 +1,22 @@
+#include "../include.hh"
+
+template<typename T>
+struct BIT {
+    int n;
+    vector<T> c;
+    BIT(size_t n) : n(n), c(n + 1) {}
+    void add(size_t i, const T& k) {
+        while (i <= n) {
+            c[i] += k;
+            i += lowbit(i);
+        }
+    }
+    T getsum(size_t i) {
+        T res = {};
+        while (i) {
+            res += c[i];
+            i -= lowbit(i);
+        }
+        return res;
+    }
+};
