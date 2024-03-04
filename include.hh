a7fc899de6b94ca6872bcd604990a52e7bc3382f
Ariel <ariel@arielherself.xyz>
2024-03-04 17:04:48 +0800

    Update include.hh

--- include.hh
+++ include.hh
@@ -28,8 +28,8 @@ using pll = pair<ll, ll>;
 /* constants */
 constexpr int INF = 0x3f3f3f3f;
 constexpr ll INFLL = 0x3f3f3f3f3f3f3f3fLL;
-constexpr ull MDL = 1e9 + 7;
-constexpr ull PRIME = 998'244'353;
+constexpr ll MDL = 1e9 + 7;
+constexpr ll PRIME = 998'244'353;
 constexpr ll MDL1 = 8784491;
 constexpr ll MDL2 = PRIME;
 
