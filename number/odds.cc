ed523135db1f4782d9b0062ff2936337e2f09d66
subcrip <contact@subc.rip>
2024-04-10 13:58:58 +0800

    Add number/odds.cc

---
+++ number/odds.cc
@@ -0,0 +1,10 @@
+auto odds = [] (ll a, ll b) -> ll {
+    if (a % 2 == 0) a += 1;
+    if (b % 2 == 0) b -= 1;
+    if (a > b) return 0;
+    ll odd_sum = ((b + 1) / 2) * ((b + 1) / 2) - ((a - 1) / 2) * ((a - 1) / 2);
+    return odd_sum;
+};
+auto oddcount = [] (ll a, ll b) -> ll {
+    return (b - a) / 2 + (a & 1 | b & 1);
+};
