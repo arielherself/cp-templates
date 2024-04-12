bd0cfba0fb4fad6c76faff0de4142f384409b019
subcrip <contact@subc.rip>
2024-04-12 09:27:25 +0800

    Add misc/set_intersection.cc
    Signed-off-by: subcrip <contact@subc.rip>

---
+++ misc/set_intersection.cc
@@ -0,0 +1,15 @@
+// Set intersection supporting std::unordered_set.
+template <typename Set, typename Key = typename Set::value_type>
+static inline Set set_intersection(const Set& lhs, const Set& rhs) {
+    if (lhs.size() <= rhs.size()) {
+        Set iset;
+        for (const Key& key : lhs) {
+            if (rhs.count(key) > 0) {
+                iset.insert(key);
+            }
+        }
+        return std::move(iset);
+    } else {
+        return set_intersection(rhs, lhs);
+    }
+}
