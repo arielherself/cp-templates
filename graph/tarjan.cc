c48002df20c737adcf35d12333099f3809f551ec
subcrip <contact@subc.rip>
2024-04-16 17:36:27 +0800

    Add graph/tarjan.cc
    Signed-off-by: subcrip <contact@subc.rip>

---
+++ graph/tarjan.cc
@@ -0,0 +1,44 @@
+namespace tarjan {
+    vector<int> scc(const vector<vector<int>>& ch) {
+        int n = ch.size();
+        int cnt = 0, scn = 0;
+        vector<int> dfn(n), low(n), vis(n), st;
+        vector<int> br(n);
+        auto tarjan = [&] (auto tarjan, int v) -> void {
+            dfn[v]=low[v]=++cnt;
+            st.push_back(v);
+            vis[v]=1;
+            for(const auto&u:ch[v])
+                if(!dfn[u]) tarjan(tarjan, u),low[v]=min(low[v],low[u]);
+                else if(vis[u])low[v]=min(low[v],dfn[u]);
+            if(dfn[v]==low[v]){
+                ++scn;
+                int u;
+                do u=st.back(), st.pop_back(),vis[u]=0,br[u]=scn; while(u!=v);
+            }
+        };
+        for (int i = 0; i < n; ++i) {
+            if (!dfn[i]) {
+                tarjan(tarjan, i);
+            }
+        }
+        return br;
+    }
+
+    vector<vector<int>> build_scc(const vector<vector<int>>& ch) {
+        int n = ch.size();
+        auto br = scc(ch);
+        int cnt = *max_element(br.begin(), br.end());
+        vector<unordered_set<int, safe_hash>> rb(cnt + 1);
+        for (int i = 0; i < n; ++i) {
+            for (auto&& u : ch[i]) {
+                rb[br[i]].emplace(br[u]);
+            }
+        }
+        vector<vector<int>> res(cnt + 1);
+        for (int i = 1; i <= cnt; ++i) {
+            res[i] = vector<int>(rb[i].begin(), rb[i].end());
+        }
+        return res;
+    }
+}
