92bc4771a873b6fb7e2938fe24e6a36d230a42c5
subcrip <contact@subc.rip>
2024-05-19 13:18:23 +0800

    Update graph/dinic.cc
    Signed-off-by: subcrip <contact@subc.rip>

--- graph/dinic.cc
+++ graph/dinic.cc
@@ -4,7 +4,7 @@ struct dinic {
         ll cap;
         ll flow;
         int rev;
-        bool mark;
+        int mark;
     };
 
     vector<vector<edge>> edges;
@@ -13,7 +13,7 @@ struct dinic {
 
     dinic(int n) : edges(n + 1), layer(n + 1), vis(n + 1) {}
 
-    void add_edge(int from, int to, ll cap, bool mark = false) {
+    void add_edge(int from, int to, ll cap, int mark = false) {
         edges[from].push_back({ to, cap, 0, int(edges[to].size()), mark });
         edges[to].push_back({ from, 0, 0, int(edges[from].size() - 1), mark });
     }
