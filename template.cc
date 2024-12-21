4a6712e156dc78e49aab51cecad137e556a9f366
arielherself <arielherself@duck.com>
2024-12-22 03:29:41 +0800

    fix long double precision error

--- template.cc
+++ template.cc
@@ -25,7 +25,7 @@ using ull = unsigned long long;
 #endif
 using int128 = __int128_t;
 using uint128 = __uint128_t;
-using ld = long double;
+using ld = __float128;  // up to 1e-9 precision in binary search
 using pii = pair<int, int>;           using pil = pair<int, ll>;           using pid = pair<int, ld>;
 using pli = pair<ll, int>;            using pll = pair<ll, ll>;            using pld = pair<ll, ld>;
 using pdi = pair<ld, int>;            using pdl = pair<ld, ll>;            using pdd = pair<ld, ld>;
